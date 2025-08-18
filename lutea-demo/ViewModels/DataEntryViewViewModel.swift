ed4db37ab90b6183b176fffd403ba099414e01e1
zmb24 <zmb24@ic.ac.uk>
2025-08-18 18:55:19 +0100

    Initialise files

---
+++ lutea-demo/ViewModels/DataEntryViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataEntryViewViewModel.swift
+//  lutea-demo
+//
+//  Created by Zayan Baig on 18/08/2025.
+//
+
+import Foundation
---
+++ lutea-demo/Views/DataEntryView.swift
@@ -0,0 +1,18 @@
+//
+//  DataEntryView.swift
+//  lutea-demo
+//
+//  Created by Zayan Baig on 18/08/2025.
+//
+
+import SwiftUI
+
+struct DataEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DataEntryView()
+}
