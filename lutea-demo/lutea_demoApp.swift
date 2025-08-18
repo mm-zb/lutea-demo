7ef113f7ffe69d0133fc79d4a09feba37854bbbc
zmb24 <zmb24@ic.ac.uk>
2025-08-18 17:14:26 +0100

    Initial Commit

---
+++ lutea-demo/lutea_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  lutea_demoApp.swift
+//  lutea-demo
+//
+//  Created by Zayan Baig on 18/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct lutea_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
