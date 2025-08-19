6fc96a97debbb161f9a1c7dfe8d190e3e668887d
zmb24 <zmb24@ic.ac.uk>
2025-08-19 12:29:17 +0100

    Initial logic for log in view model

---
+++ lutea-demo/ViewModels/HeaderViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HeaderViewViewModel.swift
+//  lutea-demo
+//
+//  Created by Zayan Baig on 18/08/2025.
+//
+
+import Foundation
---
+++ lutea-demo/Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  lutea-demo
+//
+//  Created by Zayan Baig on 18/08/2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
